415285c2eee82be7c5b2fbff9fb1d388aa941c9b
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-26 13:52:17 +0100

    added a controller to output env variables

---
+++ Fiver.Asp.Docker.Api/Controllers/EnvController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Fiver.Asp.Docker.Api.Controllers
+{
+    [Route("env")]
+    public class EnvController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var dict = new Dictionary<string, string>();
+
+            var enumerator = Environment.GetEnvironmentVariables().GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                dict.Add(enumerator.Key.ToString(), enumerator.Value.ToString());
+            }
+            
+            return Ok(dict);
+        }
+    }
+}
